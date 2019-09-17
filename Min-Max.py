ceca9be81c386595dde7e5ee82c4b79055e53bae
Vishnu Gopireddy <vishnu.var.reddy@gmail.com>
2019-09-17 20:35:59 +0530

    Update Min-Max.py

--- Min-Max.py
+++ Min-Max.py
@@ -4,7 +4,9 @@ Problem from Hackerearth on Min-Max
 '''
 
 length = raw_input()
-list_number = map(int, raw_input().split())
+
+list_number = map(int, raw_input().split()." ")
+
 total_sum = sum(list_number)
 print total_sum - max(list_number),
 print total_sum - min(list_number)
